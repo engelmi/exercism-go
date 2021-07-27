a73180869e102df679d2cc0bac607ab0d39fd49c
Michael Engel <engel.michael@hotmail.de>
2021-07-27 22:24:05 +0200

    reverse-string

---
+++ reverse-string/reverse_string.go
@@ -0,0 +1,10 @@
+package reverse
+
+func Reverse(word string) string {
+	rns := []rune(word)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	return string(rns)
+}
